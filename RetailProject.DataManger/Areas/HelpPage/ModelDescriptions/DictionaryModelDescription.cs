0bb747b796503e52da8850cc43d67c8b08d2e717
Hamed Chebil <chebil.hamed90@gmail.com>
2020-12-19 17:22:01 +0100

    initialize Solution inistialize DataManager web api project

---
+++ RetailProject.DataManger/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace RetailProject.DataManger.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ RetailProject.DataManger/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RetailProject.DataManger.WebApiApplication" Language="C#" %>
